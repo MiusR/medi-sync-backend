51c73aff05dcf500545727711abf0083e9d64b2a
Playelectro <nebo_GOD@yahoo.com>
2025-07-29 11:26:17 +0300

    test code

--- src/main/java/com/mihair/analysis_machine/AnalysisMachineApplication.java
+++ src/main/java/com/mihair/analysis_machine/AnalysisMachineApplication.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration.class
+})
 @Controller
 public class AnalysisMachineApplication {
 
