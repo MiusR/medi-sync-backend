69f532f9283a2f9911ca39701d17cc888e3fd038
Playelectro <nebo_GOD@yahoo.com>
2025-07-29 11:32:55 +0300

    just want to see this run

--- src/main/java/com/mihair/analysis_machine/AnalysisMachineApplication.java
+++ src/main/java/com/mihair/analysis_machine/AnalysisMachineApplication.java
@@ -4,16 +4,11 @@ import com.azure.security.keyvault.keys.models.KeyVaultKey;
 import com.mihair.analysis_machine.util.KeyProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@SpringBootApplication(exclude = {
-		MongoAutoConfiguration.class,
-		MongoDataAutoConfiguration.class
-})
+@SpringBootApplication
 @Controller
 public class AnalysisMachineApplication {
 
